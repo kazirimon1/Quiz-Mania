43dd67bef4c320ae182b373e6b177dafbde5b3c9
Md: Sazed Khan <manthedark233@gmail.com>
2023-12-24 21:33:05 +0600

    added new file

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-
-import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Main from './components/Main/Main';
-import Home from './components/Home/Home';
-import Topics from './components/Topics/Topics';
-import Quiz from './components/Quiz/Quiz';
-import Static from './components/Static/Static';
-import Blog from './components/Blog/Blog';
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./components/Main/Main";
+import Home from "./components/Home/Home";
+import Topics from "./components/Topics/Topics";
+import Quiz from "./components/Quiz/Quiz";
+import Static from "./components/Static/Static";
+import Blog from "./components/Blog/Blog";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <Main></Main>,
       children: [
         {
           index: true,
-          element: <Home></Home>
-
+          element: <Home></Home>,
         },
 
         {
-          path: 'topics',
+          path: "topics",
           element: <Topics></Topics>,
           loader: () => fetch(`https://openapi.programming-hero.com/api/quiz`),
         },
         {
-          path: 'quiz/:id',
+          path: "quiz/:id",
           element: <Quiz></Quiz>,
-          loader: ({ params }) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`)
+          loader: ({ params }) =>
+            fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`),
         },
         {
-          path: 'statics',
+          path: "statics",
           element: <Static></Static>,
-          loader: () => fetch(`https://openapi.programming-hero.com/api/quiz`),
         },
         {
-          path: 'blog',
-          element: <Blog></Blog>
+          path: "blog",
+          element: <Blog></Blog>,
         },
         {
-          path: '*',
-          element: <div className='text-3xl text-blue-700 mt-4'>Sorry not found please try another thing</div>
-        }
-
-
-      ]
-    }
-  ])
+          path: "*",
+          element: (
+            <div className="text-3xl text-blue-700 mt-4">
+              Sorry not found please try another thing
+            </div>
+          ),
+        },
+      ],
+    },
+  ]);
   return (
     <div className="App">
       <RouterProvider router={router}>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className=' bg-black md:flex justify-between items-center'>
-            <div className='m-4'><h2 className='text-3xl text-white'>Quiz Mania</h2></div>
-            <div>
-                <ul className='text-white flex justify-evenly gap-8 mr-12 '>
-                    <li className='hover:text-yellow-300'><Link to='/'>Home</Link></li>
-                    <li className='hover:text-yellow-300'><Link to='topics'>Topics</Link></li>
-                    <li className='hover:text-yellow-300'><Link to='statics'>Statics</Link></li>
-                    <li className='hover:text-yellow-300'><Link to='blog'>Blog</Link></li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className=" bg-black md:flex justify-between items-center">
+      <div className="m-4">
+        <h2 className="text-3xl text-white">Quiz Mania</h2>
+      </div>
+      <div>
+        <ul className="text-white flex justify-evenly gap-8 mr-12 ">
+          <li className="hover:text-yellow-300">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="hover:text-yellow-300">
+            <Link to="topics">Topics</Link>
+          </li>
+          <li className="hover:text-yellow-300">
+            <Link to="statics">Dashboard</Link>
+          </li>
+          <li className="hover:text-yellow-300">
+            <Link to="blog">Blog</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
